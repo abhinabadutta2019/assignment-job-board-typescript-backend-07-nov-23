67064fd643046c2112fd3bc527da41440fb5cc5e
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-08 13:49:51 +0530

    applyJob controller

--- src/controllers/jobController.ts
+++ src/controllers/jobController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { Job } from "../models/Job";
+import { User } from "../models/User";
 //
 interface CustomRequest extends Request {
   //as no question mark - was the reason of error
@@ -40,7 +41,44 @@ const getAllJobs = async (req: Request, res: Response) => {
   }
 };
 //
+const applyJob = async (req: CustomRequest, res: Response) => {
+  try {
+    const jobId = req.params.jobId; // Get the job ID from the route parameters
+    const userId = req.user._id; // Get the user ID from the middleware
 
+    // Check if the user is an 'applicant'
+    const user = await User.findOne({ _id: userId, userType: "applicant" });
+    if (!user) {
+      return res.status(403).json({ error: "Permission denied" });
+    }
+
+    // Check if the job has already been applied by the user
+    const job = await Job.findOne({ _id: jobId, appliedBy: userId });
+    if (job) {
+      return res.status(400).json({ error: "Job already applied" });
+    }
+
+    // Find the job and add the user to the appliedBy array
+    const updatedJob = await Job.findOneAndUpdate(
+      { _id: jobId },
+      { $addToSet: { appliedBy: userId } }, // Use $addToSet to prevent duplicates
+      { new: true }
+    );
+
+    if (!updatedJob) {
+      return res.status(404).json({ error: "Job not found" });
+    }
+
+    // Add the job ID to the user's appliedJobs array
+    user.appliedJobs.push(updatedJob._id);
+    await user.save();
+
+    res.json({ message: "Job application successful", job: updatedJob });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 //
 
-export { createJob, getAllJobs };
+export { createJob, getAllJobs, applyJob };
--- src/routers/job.ts
+++ src/routers/job.ts
@@ -6,11 +6,12 @@ import { verifyJobCreator } from "../middleware/verifyJobCreator";
 //
 import { verifyJWT } from "../middleware/verifyJWT";
 //
-import { createJob, getAllJobs } from "../controllers/jobController";
+import { createJob, getAllJobs, applyJob } from "../controllers/jobController";
 
 //
 router.post("/", verifyJobCreator, createJob);
 router.get("/", verifyJWT, getAllJobs);
+router.post("/apply/:jobId", verifyJWT, applyJob);
 //
 
 export { router as jobRouter };

0cee52ffda563c9571cc7005bdcf0abfaab286f5
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-08 12:48:09 +0530

    another middleware fore genarel auth

--- src/routers/job.ts
+++ src/routers/job.ts
@@ -4,11 +4,13 @@ const router = Router();
 //
 import { verifyJobCreator } from "../middleware/verifyJobCreator";
 //
+import { verifyJWT } from "../middleware/verifyJWT";
+//
 import { createJob, getAllJobs } from "../controllers/jobController";
 
 //
 router.post("/", verifyJobCreator, createJob);
-router.get("/", getAllJobs);
+router.get("/", verifyJWT, getAllJobs);
 //
 
 export { router as jobRouter };
